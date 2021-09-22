f4c1c1789bf25324bd92cc9e58dfda28c5a0959d
DCampbellCreative <dougcampbell1990@gmail.com>
2021-09-22 16:34:07 -0400

    Updated request place holders

--- index.js
+++ index.js
@@ -28,40 +28,66 @@ app.get('/movies/:title', (req, res) => {
 
 //returns JSON object containing data about a movies genre
 app.get('/movies/:title/:genre', (req, res) => {
-  res.send('Successful GET request returning data on one movies genre.');
+  res.send('Successful GET request returning data on one movie genres.');
 });
 
 //returns JSON object containing data about a director
-app.get('/movies/:title/:director', (req, res) => {
+app.get('/directors/:name', (req, res) => {
   res.send('Successful GET request returning data on a director.');
 });
 
 //adds user account
-app.post('/users', (req, res) => {
-  let newUser = req.body;
+// app.post('/users', (req, res) => {
+//   let newUser = req.body;
+//
+//   if(!newUser.name) {
+//     const message = 'Missing name in request body';
+//     res.status(400).send(message);
+//   } else {
+//     newUser.id = uuid.v4();
+//     users.push(newUser);
+//     res.status(201).send(newUser);
+//   }
+// });
 
-  if(!newUser.name) {
-    const message = 'Missing name in request body';
-    res.status(400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).send(newUser);
-  }
+//adds user account
+app.post('/users', (req, res) => {
+res.send('Text message showing user was successfully added.')
 });
 
 //updates user info
-app.put('/users/:name', (req, res) => {
-  let user = user.find((user) => { return user.name === req.params.name })
+// app.put('/users/:name', (req, res) => {
+//   let user = user.find((user) => { return user.name === req.params.name })
+// });
+
+//adds user account
+app.put('/users', (req, res) => {
+res.send('Text message showing user was successfully updated.')
+});
+
+//deletes a user from list
+// app.delete('/users/:name', (req, res) => {
+//   let user = user.find((user) => { return user.name === req.params.name});
+//
+//   if (user) {
+//     user = user.filter((obj) => { return obj.name !== req.params.name});
+//     res.status(201).send('User ' + req.params.name + ' was deleted.');
+//   }
+// });
 
 //deletes a user from list
 app.delete('/users/:name', (req, res) => {
-  let user = user.find((user) => { return user.name === req.params.name});
+res.send('Text message showing user was successfully deleted.')
+});
 
-  if (user) {
-    user = user.filter((obj) => { return obj.name !== req.params.name});
-    res.status(201).send('User ' + req.params.name + ' was deleted.');
-  }
+//adds movie to users favorites
+app.put('/users/:name/:favorites', (req, res) => {
+  res.send('Text message letting user know movie was successfully added to favorites');
+});
+
+//deletes from users favorites
+app.delete('/users/:name/:favorites', (req, res) => {
+  res.send('Text message letting user know movie was successfully deleted from favorites');
 });
 
 //returns default text response
