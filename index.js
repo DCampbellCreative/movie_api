ce8eda94eb6097b57396f61bf21ae10fdc35ff6b
DCampbellCreative <dougcampbell1990@gmail.com>
2021-09-29 15:33:22 -0400

    add functions for updating and deleting users by username

--- index.js
+++ index.js
@@ -39,54 +39,70 @@ mongoose.connect('mongodb://localhost:27017/movie_api',
 //creates user account
 app.post('/users', (req, res) => {
   Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-} else {
-  Users
-    .create({
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
-    })
-    .then((user) =>{res.status(201).json(user)
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    })
-  }
-})
-.catch((error) =>{
-  console.error(error);
-  res.status(500).send('Error: ' + error);
-});
+  .then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.Username + 'already exists');
+    } else {
+      Users
+      .create({
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then((user) =>{res.status(201).json(user)
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      })
+    }
+  })
+  .catch((error) =>{
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
 });
 
 //updates user info by username
 app.put('/users/:Username', (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
-  {
-    Username: req.body.Username,
-    Password: req.body.Password,
-    Email: req.body.Email,
-    Birthday: req.body.Birthday
-  }
-},
-{ new: true },
-(err, updatedUser) => {
-  if(err) {
-    console.error(err);
-    res.stauts(500).send('Error: ' + err);
-  } else {
-    res.json(updatedUser);
-  }
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true },
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.stauts(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
+
+//deletes user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+  .then((user) => {
+    if(!user) {
+      res.status(400).send(req.params.Username + ' was not found');
+    } else {
+      res.status(200).send(req.parmas.Username + ' was deleted.');
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
 //get all users
-app.get('/users', (req,res) => {
+app.get('/users', (req, res) => {
   Users.find()
   .then((users) => {
     res.status(201).json(users);
@@ -106,6 +122,22 @@ app.get('/users/:Username', (req, res) => {
   .catch((err) => {
     console.error(err);
     res.status(500).send('Error: ' + err);
+  });
+});
+
+//add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $push: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true },
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
   });
 });
 
