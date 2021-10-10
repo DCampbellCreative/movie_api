67c717e157a76e2ff7b0859acfb3754431ea7eed
DCampbellCreative <dougcampbell1990@gmail.com>
2021-10-10 17:16:09 -0400

    creates auth.js file and passport.js file

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ mongoose.connect('mongodb://localhost:27017/movie_api',
 {useNewUrlParser: true, useUnifiedTopology: true
 });
 
+//imports auth.js file
+app.use(express.urlencoded({ extended: true }));
+let auth = require('./auth')(app);
+
+const passport = require('passport');
+require('./passport');
+
 
 //get all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }),
+ (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(201).json(movies);
